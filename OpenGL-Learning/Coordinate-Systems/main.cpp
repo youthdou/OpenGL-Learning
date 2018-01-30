1aab89a21c8f3f9c96d1a5cb3dde311e1e2e1118
sww <sww@zqykj.com>
2018-01-30 15:01:22 +0800

    增加camera示例。

---
+++ OpenGL-Learning/Coordinate-Systems/main.cpp
@@ -0,0 +1,7 @@
+#if 0
+#include "main-simple.cpp"
+#endif
+
+#if 0
+#include "main-cubes.cpp"
+#endif
---
+++ OpenGL-Learning/Hello-Triangle/main.cpp
@@ -0,0 +1,20 @@
+#if 0
+#include "main-simple.cpp"
+#endif
+
+#if 0
+#include "main-VAOs.cpp"
+#endif
+
+#if 0
+#include "main-arrays.cpp"
+#endif
+
+#if 0
+#include "main-shaders.cpp"
+#endif
+
+#if 0
+#include "main-ebo.cpp"
+#endif
+
